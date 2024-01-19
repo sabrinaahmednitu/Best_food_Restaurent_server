74a9fc844c90649e73a3f7e087eb29f717d2fbfa
sabrina <sabrinaahmednitu@gmail.com>
2024-01-20 00:41:06 +0600

    all data get api

--- index.js
+++ index.js
@@ -33,40 +33,20 @@ async function run() {
       const result = await query.toArray();
       res.send(result);
     });
-
-    app.get('/product', async (req, res) => {
-      const query = restaurantCollection.find();
-      const result = await query.toArray();
-      res.send(result);
-    });
-
-    //fetch all data here
-    app.get('/product/:id([0-9a-fA-F]{24})', async (req, res) => {
-      // console.log(req.params.id);
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }; // Convert the id to ObjectId
-      const result = await restaurantCollection.findOne(query);
-      res.send(result);
-    });
-
-    app.post('/product', async (req, res) => {
-      const product = restaurantCollection.insertOne();
-      let result = await collection.findOne(product);
-      res.send(result);
-    });
   } finally {
     // Ensures that the client will close when you finish/error
-   // await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red; font-size:70; margin:20% auto; text-align:center;">Mim is my herat and my Crush and my bad grails!</h1>')
-})
+  res.send(
+    '<h1 style="color:red; font-size:70; margin:20% auto; text-align:center;">Mim is my herat and my Crush and my bad grails!</h1>'
+  );
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 // hello world
